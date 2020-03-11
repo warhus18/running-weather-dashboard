54fcf251628a3e16ca3c47098215b9ebfdf6c4bd
Jessica Gidding <jessicawarhus@jessicas-mbp.lan>
2020-03-11 07:49:24 -0400

    Simplify json file with clothing options

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,7 +3,7 @@
     <div class="container container--max-width">
       <div class="clothing-container__inner">
         <h2 class="u-margin-hug-top u-text-center">What to wear running</h2>
-        <div v-if="typeof weather.main != 'undefined' && weather.main.temp < 30">
+        <!-- <div v-if="typeof weather.main != 'undefined' && weather.main.temp < 30">
           <temp-below-freezing />
         </div>
         <div v-if="typeof weather.main != 'undefined'
@@ -24,7 +24,20 @@
         </div>
         <div v-if="typeof weather.main != 'undefined' && weather.main.temp > 70">
           <temp-seventies-above />
-        </div>
+        </div> -->
+
+          <ol class="u-list-unstyled container__grid container__grid--3col clothing-list__container">
+            <li class="clothing-list__item" v-for="item in clothingItems" v-bind:key="item">
+              <div class="u-text-center">
+                <div class="clothing-list__img">
+                  <img src="https://via.placeholder.com/100x100" srcset="https://via.placeholder.com/100x100 1x, https://via.placeholder.com/200x200  2x" alt="Alternative Text Describing the Image">
+                </div>
+                <span class="clothing-list__text">{{ item.text }}</span>
+              </div>
+            </li>
+            <!-- /. clothing-list__item -->
+          </ol>
+          <!-- /.clothing-list__container -->
       </div>
     </div>
   </section>
@@ -32,22 +45,30 @@
 </template>
 
 <script>
-import BelowFreezing from '@/components/temperature/BelowFreezing';
-import Thirties from '@/components/temperature/Thirties';
-import Forties from '@/components/temperature/Forties';
-import Fifties from '@/components/temperature/Fifties';
-import Sixties from '@/components/temperature/Sixties';
-import SeventiesAbove from '@/components/temperature/SeventiesAbove';
+// import BelowFreezing from '@/components/temperature/BelowFreezing';
+// import Thirties from '@/components/temperature/Thirties';
+// import Forties from '@/components/temperature/Forties';
+// import Fifties from '@/components/temperature/Fifties';
+// import Sixties from '@/components/temperature/Sixties';
+// import SeventiesAbove from '@/components/temperature/SeventiesAbove';
+
+import clothing from '@/assets/clothing.json';
 
 export default {
   props: ['weather'],
-  components: {
-    'temp-below-freezing': BelowFreezing,
-    'temp-thirties': Thirties,
-    'temp-forties': Forties,
-    'temp-fifties': Fifties,
-    'temp-sixties': Sixties,
-    'temp-seventies-above': SeventiesAbove,
-  },
+  data() {
+    return {
+      clothingItems: clothing
+    }
+  }
+  
+  // components: {
+  //   'temp-below-freezing': BelowFreezing,
+  //   'temp-thirties': Thirties,
+  //   'temp-forties': Forties,
+  //   'temp-fifties': Fifties,
+  //   'temp-sixties': Sixties,
+  //   'temp-seventies-above': SeventiesAbove,
+  // },
 };
 </script>
